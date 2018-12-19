92606ed871d31eaf9e7d7f5be868d3f6d28a8de8
Luis Zabala <luis.zabala076@gmail.com>
2018-12-19 02:59:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown (seconds)
-  while seconds > 0
-  puts "#{seconds} SECOND(S)!"
-  counter -= 1
+def countdown (number)
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
 end
 "HAPPY NEW YEAR!"
 end
